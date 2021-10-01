5e9612a049125c54d0298cfee96ecb4cbf78dbb6
Rebecca Simms <rebecca.simms@thehutgroup.com>
2021-10-01 14:34:29 +0100

    First commit

---
+++ src/main/java/com/thehutgroup/App.java
@@ -0,0 +1,14 @@
+package com.thehutgroup;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Game game = new Game();
+        System.out.println("Game score: " + game.score());
+    }
+}
---
+++ src/main/java/com/thehutgroup/Game.java
@@ -0,0 +1,13 @@
+package com.thehutgroup;
+
+public class Game {
+
+    public int roll(int pins) {
+        return 10;
+    }
+
+    public int score() {
+        return 0;
+    }
+
+}
